e7802ff9e9d9238d6279cc50d71000166ba7583d
Vadimatorik <Deryabkinda@mail.ru>
2017-09-23 00:36:20 +0700

    Починил WDT.

--- wdt/wdt.cpp
+++ wdt/wdt.cpp
@@ -1,37 +1,33 @@
 #include "wdt.h"
+#include "rcc.h"
 
 //**********************************************************************
 // Доступные пользователю функции.
 //**********************************************************************
 void wdt::init ( void ) const {
-    volatile uint32_t sr;
-
     *((uint32_t *) 0xE0042008)  = 0x1DFF;				// Если процессор находится в состоянии halt, watchdog будет стоять и не мешать отладке.
 
-    IWDG->K = 0xCCCC;									// Запускаем сторожевой таймер, таймер стартует с значением по умолчанию 0xFFF.
+    IWDG->K = 0xCCCC;
+    IWDG->K = 0x5555;
+    IWDG->P = 0x6;
+    IWDG->RL = 32 * this->cfg->startup_time_ms / 256;
 
-    IWDG->K = 0x5555;									// Ключ, разрешающий запись.
-    while ( IWDG->S & ( M_EC_TO_U32( EC_WDT_S_MSK::PVU ) |
-                        M_EC_TO_U32( EC_WDT_S_MSK::RVU ) ) );	// Ждем, чтобы операция загрузки значения завершилась,
-                                                        // прескалер был готов к обновлению.
-    IWDG->P = 0x6;										// Деление частоты watchdog (40кгц) на 256
-    while ( true ) {
-        sr = IWDG->S;
-        if ( ! ( sr & M_EC_TO_U32( EC_WDT_S_MSK::RVU ) ) )  break;
-    }
-    IWDG->RL = 40 * this->cfg->startup_time_ms / 256;
+    while( IWDG->S );
+
+    IWDG->K = 0xAAAA;                                   // Перезагружаем WDT.
+
     USER_OS_STATIC_TASK_CREATE( this->task, "wdt_th", F2_WDT_TASK_STACK_SIZE, ( void* )this, this->cfg->task_prio, this->task_stack, &this->task_struct );
 }
 
 void wdt::reset ( void ) const {
-    IWDG->K = 0x5555;									// Rлюч, разрешающий запись.
-    IWDG->RL = 40 * this->cfg->run_time_ms / 256;
+    IWDG->K = 0x5555;									// Ключ, разрешающий запись.
+    IWDG->RL = 32 * 10 * this->cfg->run_time_ms / 256;
     IWDG->K = 0xAAAA;									// Ключ, перезагружающий таймер.
 }
 
 void wdt::reset_service	( void ) const {
     IWDG->K = 0x5555;									// Ключ, разрешающий запись.
-    IWDG->RL = 400UL *  this->cfg->service_time_ms / 256;
+    IWDG->RL = 32 * 10 * this->cfg->service_time_ms / 256;
     IWDG->K = 0xAAAA;									// Ключ, перезагружающий таймер.
 }
 
@@ -41,7 +37,7 @@ void wdt::reset_service	( void ) const {
 void wdt::task ( void* p_obj ) {
     wdt* obj	=	(wdt *)p_obj;
     USER_OS_TICK_TYPE last_wake_time;
-    const USER_OS_TICK_TYPE time_out = 100;
+    const USER_OS_TICK_TYPE time_out = 10;
     last_wake_time = USER_OS_TASK_GET_TICK_COUNT();
     while ( true ) {
         configASSERT( !obj->reboot );					// Если задана перезагрузка - виснем!
--- wdt/wdt_struct.h
+++ wdt/wdt_struct.h
@@ -1,6 +1,5 @@
-#include <stdint.h>
-
 #include "f2_api_conf.h"
+#include <stdint.h>
 
 struct __attribute__( ( packed ) ) iwdg_struct {
     uint32_t    K;
@@ -9,7 +8,7 @@ struct __attribute__( ( packed ) ) iwdg_struct {
     uint32_t    S;
 };
 
-#define IWDG        ( ( iwdg_struct* )( 0x40003000 ) )
+#define IWDG        ( ( volatile iwdg_struct* )( 0x40003000 ) )
 
 enum class EC_WDT_S_POS {
     PVU         = 0,
@@ -17,6 +16,6 @@ enum class EC_WDT_S_POS {
 };
 
 enum class EC_WDT_S_MSK {
-    PVU         = 0b111     << M_EC_TO_U8( EC_WDT_S_POS::PVU ),
+    PVU         = 1         << M_EC_TO_U8( EC_WDT_S_POS::PVU ),
     RVU         = 1         << M_EC_TO_U8( EC_WDT_S_POS::RVU )
 };
